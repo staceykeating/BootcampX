f04e089ad6efc286f41cb6769c427f8c1225e3b2
Stacey Keating <staceyamberkk@gmail.com>
2020-04-27 23:39:51 +0000

    joining tables

---
+++ 2_queries/0_rollover_students.sql
@@ -0,0 +1,6 @@
+SELECT students.name AS student_name, cohorts.name AS cohort_name, cohorts.start_date AS cohort_start_date, students.start_date AS student_start_date
+FROM students 
+JOIN cohorts ON  cohorts.id = cohort_id
+WHERE cohorts.start_date !=  students.start_date
+ORDER BY cohort_start_date;
+
